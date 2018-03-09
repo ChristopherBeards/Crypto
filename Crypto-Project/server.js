87a92c8733428dac6f4a445bcea6eded21668ce0
Christopher Beards <beardscm@gmail.com>
2018-03-09 08:29:56 -0500

    Prints Current and Previous Prices
    Prints Difference

--- Crypto-Project/server.js
+++ Crypto-Project/server.js
@@ -25,15 +25,36 @@ server.listen(3030, (err) => {
 server.get('/compare', (req, res) => {
  let current = 0;
  let previous = 0;
- request('https://api.coindesk.com/v1/bpi/currentprice.json', (error, response, body) => {
+
+ request('https://api.coindesk.com/v1/bpi/currentprice/USD.json', (error, response, body) => {
     let parseObject = JSON.parse(body);
-    current = parseObject.bpi.USD.rate   
+    current = parseObject.bpi.USD.rate_float  
+ 
   });
+
   request('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday', (error, response, body) => {
     let parseObject = JSON.parse(body);
     previous = Object.values(parseObject.bpi); 
-    
-    res.json({ current, previous: `${previous[0]}` });
+    previous = previous[0];
+    let difference = current - previous;
+    // console.log(previous - current)
+
+    // const prices = {
+    //   current: current,
+    //   previous: previous[0]
+    // }
+    // result = null;
+    // if (current > previous) {
+    //   console.log(current, previous)
+    //   result = 'The value has risen by $' + current - previous;
+    // }
+    // else {
+    //   result = 'The value has fallen by $' + current - previous;
+    // }
+    // console.log(result)
+
+    res.send({ current, previous, difference });
+    // res.json({ current, previous: `${previous[0]}` });
   });
  
  
